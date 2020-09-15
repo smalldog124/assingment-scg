0bd0812ddffaea6bf2a36d131142a588a452ea79
smalldog124 <panumars124@gmail.com>
2020-09-15 22:18:44 +0700

    added: เพิ่ม api path /api/doscg/getXYZ ใน backend

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,13 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.status(200).json({'massages':'already'});
+    res.status(200).json({ 'massages': 'already' });
 });
 
+app.post('/api/doscg/getXYZ', (req, res) => {
+    const body = req.body;
+    res.status(200).json({x:1,y:3,z:33})
+})
+
 const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Listening on port${port}...`) );
+app.listen(port, () => console.log(`Listening on port${port}...`));
