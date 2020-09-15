06550e83b61f559a9d9fdbe37faada6fd0b4c6b6
smalldog124 <panumars124@gmail.com>
2020-09-15 22:21:05 +0700

    added: เพิ่ม api path /api/doscg/getBC ของ backend

--- backend/index.js
+++ backend/index.js
@@ -12,5 +12,10 @@ app.post('/api/doscg/getXYZ', (req, res) => {
     res.status(200).json({x:1,y:3,z:33})
 })
 
+app.post('/api/doscg/getBC', (req, res) => {
+    const body = req.body;
+    res.status(200).json({b:2,c:-42})
+})
+
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port${port}...`));
