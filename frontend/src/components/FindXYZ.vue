ab8d06283a718dd404f20ce31860bfd15a1356f8
smalldog124 <panumars124@gmail.com>
2020-09-19 04:52:52 +0700

    added: เพิ่มให้เรียกไปใช้ api backend

---
+++ frontend/src/components/FindXYZ.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h3>X, Y, 5, 9, 15, 23, Z - Please create a new function for finding X, Y, Z value</h3>
+    <button @click="getXYZ">Anser</button>
+    <div id="anser">
+      X: {{x}}
+      <br />
+      Y: {{y}}
+      <br />
+      Z: {{z}}
+      <br />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "FindXYZ",
+  data() {
+    return {
+      x: null,
+      y: null,
+      z: null,
+    };
+  },
+  methods: {
+    getXYZ() {
+      var body = { missing_list: ["", "", 5, 9, 15, 23, ""] };
+      axios.post("http://localhost:3000/api/doscg/getXYZ", body).then((res) => {
+        this.x = res.data.x;
+        this.y = res.data.y;
+        this.z = res.data.z;
+      });
+    },
+  },
+};
+</script>
