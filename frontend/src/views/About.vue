a162fb7d496623e62aa7671500dec08d389b0935
smalldog124 <panumars124@gmail.com>
2020-09-19 10:32:49 +0700

    added: เพิ่มรูปภาพในหน้า about

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,38 +1,18 @@
 <template>
-  <div>
-    <h3>If A = 21, A + B = 23, A + C = -21 - Please create a new function for finding B and C value</h3>
-    <button @click="getBC">Anser</button>
-    <div id="anser">
-      B: {{b}}
-      <br />
-      C: {{c}}
-      <br />
-    </div>
+  <div class="profile">
+   <img id="profile-image" alt="profile" src="../assets/profile.jpg" />
   </div>
 </template>
 
 <script>
-import axios from "axios";
 export default {
-  name: "FindBC",
-  data() {
-    return {
-      b: null,
-      c: null,
-    };
-  },
-  methods: {
-    getBC() {
-      var body = {
-        a: 21,
-        ans_b: 23,
-        ans_c: -21,
-      };
-      axios.post("http://localhost:3000/api/doscg/getBC", body).then((res) => {
-        this.b = res.data.b;
-        this.c = res.data.c;
-      });
-    },
-  },
+  name: "Aboutme"
 };
 </script>
+
+<style scoped>
+ #profile-image{
+   border-radius: 50%;
+   width: 10%;
+ }
+</style>
