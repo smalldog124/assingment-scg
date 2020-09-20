d508d67a8692fdfddfc19b0f10023aed4315e205
smalldog124 <panumars124@gmail.com>
2020-09-20 13:34:52 +0700

    Added: เพิ่มข้อมูลช่องทางการติดต่อ

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -8,12 +8,16 @@
         I have more than 2 yers' experience building software for Bank industry
         and use Agile is development process(extreme programming + scrum).
         I spcialised in backend devilopment for complex scalable APIs.
-        I write about software development on <a href="#">my GitHub</a>
+        I write about software development on
+        <a href="#">my GitHub</a>
       </div>
       <div>
-        <h3>My Skills </h3>
+        <h3>My Skills</h3>
         <div>
-          <img id="golnag-logo" src="https://i.pinimg.com/236x/ca/1f/74/ca1f746d6f232f87fca4e4d94ef6f3ab--app-technology.jpg">
+          <img
+            id="golnag-logo"
+            src="https://i.pinimg.com/236x/ca/1f/74/ca1f746d6f232f87fca4e4d94ef6f3ab--app-technology.jpg"
+          />
           <h4>Golang</h4>
           <ul>
             <li>gin-gonic Framework</li>
@@ -22,7 +26,10 @@
           </ul>
         </div>
         <div>
-          <img id="docker-logo" src="https://network.cmu.ac.th/wiki/images/e/e6/Docker_facebook_share.png">
+          <img
+            id="docker-logo"
+            src="https://network.cmu.ac.th/wiki/images/e/e6/Docker_facebook_share.png"
+          />
           <h4>Docker</h4>
           <ul>
             <li>docker-compose</li>
@@ -31,13 +38,29 @@
           </ul>
         </div>
         <div>
-          <img id="nodejs-logo" src="https://dd.engineering/blog/how-to-set-up-a-back-end-project-using-typescript-and-node-js/banner.jpg">
+          <img
+            id="nodejs-logo"
+            src="https://dd.engineering/blog/how-to-set-up-a-back-end-project-using-typescript-and-node-js/banner.jpg"
+          />
           <h4>Node.js & Typscript</h4>
           <ul>
             <li>Express</li>
             <li>Axios</li>
           </ul>
-        </div>  
+        </div>
+      </div>
+      <div>
+        <h3>Contact</h3>
+        <p>Email: panumars124@gmail.com</p>
+        <p>
+          Facebook:
+          <a href="https://web.facebook.com/panumarss/">facebook.com/panumarss</a>
+        </p>
+        <p>
+          Medium:
+          <a href="https://medium.com/@panumars124">medium.com/@panumars124</a>
+        </p>
+        <p>Phone no: 088-3564267</p>
       </div>
     </div>
   </div>
