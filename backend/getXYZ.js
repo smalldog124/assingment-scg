92eb682307c3794be2f1302e6c3a8c4ff3fd980f
smalldog124 <panumars124@gmail.com>
2020-09-20 12:45:38 +0700

    Edited: ย้ายฟังก์ชัน getXYZ ออกไปเป็นไฟล์

---
+++ backend/getXYZ.js
@@ -0,0 +1,8 @@
+function getXYZ(missing_list){
+    const x = 1
+    const y = 1+(2*(2-1))
+    const z = 23 + (2*(7-1))
+    return {x,y,z}
+}
+
+module.exports = getXYZ
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const axios = require('axios').default;
+const getXYZ = require('./getXYZ.js');
 
 const app = express();
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
 
 app.get('/', (req, res) => {
@@ -16,26 +17,27 @@ app.get('/', (req, res) => {
 
 app.post('/api/doscg/getXYZ', (req, res) => {
     const body = req.body;
-    res.status(200).json({x:1,y:3,z:33})
+    res.status(200).json(getXYZ(body));
 })
 
 app.post('/api/doscg/getBC', (req, res) => {
     const body = req.body;
-    res.status(200).json({b:2,c:-42})
+    res.status(200).json({ b: 2, c: -42 })
 })
 
 app.post('/api/doscg/getRoute', (req, res) => {
     const body = req.body;
-    const url = 'http://localhost:8080/maps/api/directions/json?origin='+body.origin+'&destination='+body.destination+'&mode=DRIVING&key='+google_key;
-    axios.get(url).then((response)=>{
+    const url = google_enpoint + '/maps/api/directions/json?origin=' + body.origin + '&destination=' + body.destination + '&mode=DRIVING&key=' + google_key;
+    axios.get(url).then((response) => {
         let mgs = JSON.parse(response.data);
         res.status(200).json(mgs);
-    }).catch((error)=>{
+    }).catch((error) => {
         console.log(error)
         res.status(500);
     })
 })
 
 const port = process.env.PORT || 3000
-const google_key = process.env.GGKEY || 'YOURKEY'
+const google_key = process.env.GGKEY || '$YOURKEY'
+const google_enpoint = process.env.GGENPONT || 'http://localhost:8001'
 app.listen(port, () => console.log(`Listening on port${port}...`));
